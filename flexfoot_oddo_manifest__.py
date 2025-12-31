8fbaf85954ea6fd07857da733f06b62d16dc23c7
Ankush Kumar <ksaw44885@gmail.com>
2025-12-31 13:33:29 +0530

    Update flexfoot_oddo_manifest__.py

--- flexfoot_oddo_manifest__.py
+++ flexfoot_oddo_manifest__.py
@@ -1,1 +1,19 @@
-
+{
+    'name': 'FlexFoot Ecommerce',
+    'version': '1.0',
+    'category': 'Website',
+    'summary': 'Online Shoe Store using Odoo',
+    'description': 'An e-commerce website for selling shoes online.',
+    'author': 'Ankush Kumar',
+    'depends': ['website', 'website_sale'],
+    'data': [
+        'views/templates.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'flexfoot_odoo_module/static/src/css/style.css',
+        ],
+    },
+    'installable': True,
+    'application': True,
+}
